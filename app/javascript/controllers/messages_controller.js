15afae34237ba698cd58a356e7ca748096e42701
keit1722 <keit1722@gmail.com>
2024-05-07 06:29:25 +0900

    新しいメッセージまでスクロール

--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -2,7 +2,13 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="messages"
 export default class extends Controller {
+  static targets = ["broadcastMessage"];
+
   connect() {
     this.element.scrollTop = this.element.scrollHeight;
+  }
+
+  broadcastMessageTargetConnected(element) {
+    element.scrollIntoView();
   }
 }
